c738fc5dd55cbd5b489268fa63de4bae4c0a95ab
johannsl <johannsl@stud.ntnu.no>
2015-08-30 16:43:37 +0200

    began work on a_star.py

---
+++ project_module_1/a_star.py
@@ -0,0 +1,18 @@
+#Written by johannsl 2015
+
+#Astar class
+class Astar:
+    def __init__(self, grid, option):
+        self.grid = grid
+        self.option = option
+
+    #This method incrementally solves the problem
+    def incremental_solver(self):
+
+    #This method completely solves the problem
+    def complete_solver(self):
+
+        #Initialize the algorithm
+        closedList = []
+        openList = []
+        self.grid.
