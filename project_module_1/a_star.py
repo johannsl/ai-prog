319b8c1aa047118cdcf777eabfc206683af65169
johannsl <johannsl@stud.ntnu.no>
2015-09-01 16:43:34 +0200

    Changed grid build. Deattached the gui. Written complete_solver in a_star

--- project_module_1/a_star.py
+++ project_module_1/a_star.py
@@ -2,8 +2,8 @@
 
 #A star class
 class AStar:
-    def __init__(self, grid, search_type, distance_type):
-        self.grid = grid
+    def __init__(self, graph, search_type, distance_type):
+        self.graph = graph
         self.search_type = search_type
         self.distance_type = distance_type
 
@@ -17,36 +17,113 @@ class AStar:
         #Initialize the algorithm
         closed_list = []
         open_list = []
-        n0 = self.grid.grid[self.grid.a_pos_x][self.grid.a_pos_y]
+        n0 = self.graph.grid[self.graph.a_pos_x][self.graph.a_pos_y]
         n0.g = 0
-
-        #Check which method of calculating h to use, then set h and f
-        if self.distance_type == "manhattan distance":
-            n0.h = calculate_manhattan_distance(n0)
-        else: n0.h = calculate_euclidian_distance(n0)
-        n0.g + n0.h = n0.f
+        n0.h = self.calculate_h(n0)
+        n0.f = n0.g + n0.h
         open_list.append(n0)
 
-        #Initiate main loop
+        #Initiate agenda loop
         while open_list:
-            x = open_list.pop()
-            closed_list.append(x)
+
+            #Sort the open_list according to search method, pop a node, and add it to closed_list
+            if self.search_type == "best-first":
+                open_list.sort(key=lambda x: x.f, reverse=True)
+
+                X = open_list.pop()
+            elif self.search_type == "breadth-first":
+                X = open_list.pop(0)
+            elif self.search_type == "depth-first":
+                X = open_list.pop()
+            else:
+                return "ERROR: search_type"
+            closed_list.append(X)
+            #print X.pos_x, X.pos_y
 
             #Look for end properties
-            if x.tag == "B":
-                path = []
-                retracePath(path, x)
-                return 
+            if X.tag == "B":
+                path = self.retrace_path(X, [X])
+                print path
+                for i in path:
+                    print i.pos_x, i.pos_y
+                return "SUCSESS"
 
-            #Look for neighbour nodes
+            #Generate a list of successor nodes to a node X
+            successors = self.graph.generate_all_successors(X) 
 
+            #Check whether nodes have been visited before. Update the ones that has. Add the rest to open_list
+            for S in successors:
+                node_found = False
+                X.kids.append(S)
+                if S not in closed_list:
+                    if S not in open_list:
+                        self.attach_and_eval(S, X)
+                        open_list.append(S)
+                    elif X.g + self.graph.calculate_arc_cost(X, S) < S.g:
+                        self.attach_and_eval(S, X)
+                        self.propagate_path_improvements(S)
+                elif X.g + self.graph.calculate_arc_cost(X, S) < S.g:
+                    self.attach_and_eval(S, X)
 
+                #for c in closed_list:
+                #    if c == s:
+                #        if c.f > s.f:
+                #            c.parent = s.parent
+                #            c.g = s.g
+                #            #path_improve(s)
+                #        node_found = True
+                #        break
+                #if node_found == True:
+                #    continue
+                #for o in open_list:
+                #    if o == s:
+                #        if o.f > s.f:
+                #            o.parent = s.parent
+                #            o.g = s.g
+                #        node_found = True
+                #if node_found == True:
+                #    continue
+                #
+                #open_list.append(s)
 
-    #This method calculates h using manhattan distance
-    def calculate_manhattan_distance(self, ):
-        return
-
-    #This method calculates h using euclidian distance
-    def calculate_euclidian_distance(self):
-        return
-
+        #If there are no more nodes in open_list, there is no solution        
+        return "FAIL"
+    
+    #This method calculates the h value depending on distance_type
+    def calculate_h(self, node):
+        if self.distance_type == "manhattan distance":
+            x_distance = abs(self.graph.b_pos_x-node.pos_x)
+            y_distance = abs(self.graph.b_pos_y-node.pos_y)
+            manhattan_distance = x_distance + y_distance
+            return manhattan_distance
+        elif self.distance_type == "euclidian distance":
+            raise NotImplementedError 
+        else:
+            return
+    
+    #This method sets the parent, g, h, and f value of a node C
+    def attach_and_eval(self, C, P):
+        C.parent = P
+        C.g = P.g + self.graph.calculate_arc_cost(P, C)
+        C.h = self.calculate_h(C)
+        C.f = C.g + C.h
+    
+    #This method recursively improves the path of all kid nodes of node P
+    def propagate_path_improvements(self, P):
+        for C in P.kids:
+            if P.g + self.graph.calculate_arc_cost(P, C) < C.g:
+                C.parent = P
+                C.g = P.g + self.graph.calculate_arc_cost(P, C)
+                C.f = C.g + C.h
+                propagate_path_improvements(self, C)
+                
+    #This method recursively finds the path from B to A and returns it as a list
+    def retrace_path(self, N, path):
+        if N.tag == "A":
+            print "lol"
+            print path
+            return path
+        else:
+            path.append(N.parent)
+            return self.retrace_path(N.parent, path)
+         
--- project_module_1/problem_specific.py
+++ project_module_1/problem_specific.py
@@ -36,8 +36,9 @@ Premade problems are:"""
 
     #This is the mainloop - It reads input from the user
     while flag:
-        the_input = raw_input(" > ")
-
+        #the_input = raw_input(" > ")
+        the_input = "Run 2"
+        
         #Run a premade grid
         if the_input != "Run new" and the_input.startswith("Run"):
 
@@ -57,7 +58,7 @@ Premade problems are:"""
             if line_list[1] <= 50 and line_list[2] <=30:
                 premade_grid = Grid(columns=line_list[1], rows=line_list[2], a_pos_x=line_list[3], a_pos_y=line_list[4], b_pos_x=line_list[5], b_pos_y=line_list[6], walls= walls)
                 premade_grid_gui = GUI(grid = premade_grid)
-                _run_gui(premade_grid_gui)
+                #_run_gui(premade_grid_gui)
             else: print "Error: Grid too large"
             flag = False
             f.close()
@@ -118,8 +119,9 @@ class Grid:
         for c in range(columns):
             row = []
             for r in range(rows):
-                row.append(Node(tag="O", g=None, h=None, f=None, pos_x=c, pos_y=r, parent=None, kids=None))
+                row.append(Node(tag="O", g=None, h=None, f=None, pos_x=c, pos_y=r, parent=None, kids=[]))
             grid.append(row)
+        self.grid = grid
         
         #Set node tags
         grid[a_pos_x][a_pos_y].tag = "A"
@@ -130,9 +132,40 @@ class Grid:
                     grid[c][r].tag = "X"
         
         #Initialize the grid of nodes and flip the grid so it fits the exercice
-        grid = zip(*grid)[::-1]
-        self.grid = grid
-        
+        #ISSUE: When to flip the board?
+        #grid = zip(*grid)[::-1]
+
+        for r in range(rows):
+            row = []
+            for c in range(columns):
+                row.append(grid[c][r].tag)
+            print row
+
+    #Find succeessors to a node in the grid and add them to a clockwise list
+    def generate_all_successors(self, node):
+        successors = []
+        if node.pos_x > 0:
+            left = self.grid[node.pos_x-1][node.pos_y]
+            if left.tag is not "X":
+                successors.append(left)
+        if node.pos_y < self.rows-1:
+            below = self.grid[node.pos_x][node.pos_y+1]
+            if below.tag is not "X":
+                successors.append(below)
+        if node.pos_x < self.columns-1:
+            right = self.grid[node.pos_x+1][node.pos_y]
+            if right.tag is not "X":
+                successors.append(right)
+        if node.pos_y > 0:
+            above = self.grid[node.pos_x][node.pos_y-1]
+            if above.tag is not "X":
+                successors.append(above)
+        return successors
+    
+    #Find the distance between a node C
+    def calculate_arc_cost(self, C, P):
+        return 1
+
 #GUI is a subclass of Tkinter
 class GUI(tk.Tk):
 
@@ -146,9 +179,9 @@ class GUI(tk.Tk):
         execmenu = tk.Menu(menubar)
         execmenu.add_command(label="Best-first search", command=self.best_first_search)
         execmenu.add_separator()
-        execmenu.add_command(label="Depth-first search", command=self.depth_first_search)
-        execmenu.add_separator()
         execmenu.add_command(label="Breadth-first search", command=self.breadth_first_search)
+        execmenu.add_separator()
+        execmenu.add_command(label="Depth-first search", command=self.depth_first_search)
         execmenu.add_separator()
         menubar.add_cascade(label="Exec", menu=execmenu)
         self.config(menu=menubar)
@@ -180,19 +213,22 @@ class GUI(tk.Tk):
         #Place the window in the topmost left corner to prevent glitches in the gui
         self.canvas.xview_moveto(0)
         self.canvas.yview_moveto(0)
+
+        self.best_first_search()
     
     #Run best-first search
     def best_first_search(self):
         search = a_star.AStar(self.grid, "best-first", "manhattan distance")
-        search.complete_solver()
+        print(search.complete_solver())
+
+    #Run breadth-first search
+    def breadth_first_search(self):
+        raise NotImplementedError
 
     #Run depth-first search
     def depth_first_search(self):
         raise NotImplementedError
 
-    #Run breadth-first search
-    def breadth_first_search(self):
-        raise NotImplementedError
     
     #Redraw the gui
     def redraw():
