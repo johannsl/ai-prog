3b13221f4219b9c4e035665a1bb754d3f5ed51ba
Iver Egge <iverasp@gmail.com>
2015-10-06 00:09:06 +0200

    moved agac out of gui

---
+++ module_2/a_star_gac.py
@@ -0,0 +1,106 @@
+__author__ = 'iver'
+
+from a_star_csp import AStarCSP
+from csp import CSP
+from copy import deepcopy
+import heapq
+from operator import attrgetter
+
+
+class AStarGAC:
+    def __init__(self, graph, domain_size):
+        self.graph = graph
+        self.domain_size = domain_size
+        self.astar = None
+        self.csp = None
+        self.astar = AStarCSP(self.graph)
+        self.csp = CSP(self.graph)
+        self.nodes_checked = set()
+        self.goal_node = None
+
+
+    def makefunc(self, var_names, expression, envir=globals()):
+        args = ""
+        for n in var_names: args = args + "," + n
+        return eval("(lambda " + args[1:] + ": " + expression + ")"
+                    , envir)
+
+    def initialize(self):
+        # Initiate csp
+        domain = []
+        g = self.makefunc(["x", "y"], "x != y")
+        for i in range(self.domain_size):
+            domain.append(i)
+        for i in range(self.graph.nv):
+            self.csp.add_variable(name=i, domain=domain)
+        for vertex, other_vertex in self.graph.edges_dict.iteritems():
+            for other_vertex in other_vertex:
+                self.csp.add_constraint_one_way(vertex, other_vertex, g)
+                self.csp.add_constraint_one_way(other_vertex, vertex, g)
+
+        self.csp.initialize()
+        self.astar.initialize(distance_type=None)
+
+        # Refine n0
+        csp_result = self.csp.domain_filtering_loop()
+        print csp_result[0]
+        if self.csp.contradictory:
+            print "Error in problem"
+            return
+        elif self.csp.is_solved():
+            print "Solution found"
+            return "SOL"
+        else:
+            self.astar.n0.domains = deepcopy(self.csp.domains)
+            return "SOL"
+        return
+
+    # Return
+    # "MOD" if we modified the domain
+    # "SOL" if we solved the puzzle
+    # "NON" if no change has happened
+    def increment(self):
+        # run astar
+        if self.csp.is_solved():
+            print "Solution found!"
+            return "SOL"
+        astar_result = self.astar.incremental_solver()
+        if astar_result[0].startswith("SUCCESS"):
+            current_csp_domains = deepcopy(self.csp.domains)
+            found_better = False
+
+            for node in self.astar.open_heap:
+                if node not in self.nodes_checked: self.nodes_checked.add(node)
+                else: continue
+                self.csp.contradictory = False
+                # rerun csp and find best guess
+                self.csp.domains = node.domains
+                csp_rerun_result = self.csp.rerun()
+                if self.csp.is_solved():
+                    print "Solution found"
+                    self.goal_node = node
+                    return "SOL"
+                if self.csp.contradictory:
+                    self.astar.open_heap.remove(node)
+                    self.astar.open_set.remove(node)
+                    continue
+                else:
+                    # fix node
+                    node.domains = deepcopy(self.csp.domains)
+                    found_better = True
+                    node.set_f(g=0, h=self.astar.calculate_h(node))
+            if found_better:
+                node = min(self.astar.open_heap, key=attrgetter('f'))
+                #node = self.astar.open_heap[0]
+                #print "best f", node.f
+                self.csp.domains = node.domains
+                self.goal_node = node
+                return "MOD"
+            else:
+                self.csp.domains = current_csp_domains
+
+            heapq.heapify(self.astar.open_heap)
+            return "NON"
+
+
+        else: return "NON"
--- module_2/gui.py
+++ module_2/gui.py
@@ -1,10 +1,5 @@
-import copy
-import csp
-import heapq
 import Tkinter as tk
-from copy import deepcopy
-from operator import attrgetter
-from a_star_csp import AStarCSP
+from a_star_gac import AStarGAC
 
 
 # GUI is an interface subclass of Tkinter
@@ -12,17 +7,12 @@ class GUI(tk.Tk):
     def __init__(self, graph):
         tk.Tk.__init__(self)
         self.graph = graph
-        self.astar = None
-        self.csp = None
 
         # constants
         self.graph_size = 800.0
         self.vertex_size = 15.0
         self.update_speed = 1
         self.color_list = ("red", "medium blue", "yellow", "orange", "sea green", "brown", "purple", "white", "black", "violet")
-
-        self.nodes_checked = set()
-        self.goal_node = None
 
         # Create the menu
         menubar = tk.Menu(self)
@@ -63,125 +53,44 @@ class GUI(tk.Tk):
         self.canvas.xview_moveto(0)
         self.canvas.yview_moveto(0)
 
-    def makefunc(self, var_names, expression, envir=globals()):
-        args = ""
-        for n in var_names: args = args + "," + n
-        return eval("(lambda " + args[1:] + ": " + expression + ")"
-                    , envir)
-
     def execute(self, domain_size):
         self.canvas.itemconfig("oval", fill="gray80")
-        self.astar = AStarCSP(self.graph)
-        self.csp = csp.CSP(self.graph)
-        self.astar.distance_type = "csp"
-
-        # Initiate csp
-        domain = []
-        g = self.makefunc(["x", "y"], "x != y")
-        for i in range(domain_size):
-            domain.append(i)
-        for i in range(self.graph.nv):
-            self.csp.add_variable(name=i, domain=domain)
-        for vertex, other_vertex in self.graph.edges_dict.iteritems():
-            for other_vertex in other_vertex:
-                self.csp.add_constraint_one_way(vertex, other_vertex, g)
-                self.csp.add_constraint_one_way(other_vertex, vertex, g)
-        self.csp.initialize()
-
-        # Initiate astar
-        self.astar.initialize(distance_type="csp")
-
-        # Refine n0
-        csp_result = self.csp.domain_filtering_loop()
-        print csp_result[0]
-        if self.csp.contradictory:
-            print "Error in problem"
-            return
-        elif self.csp.is_solved():
-            print "Solution found"
+        self.astargac = AStarGAC(self.graph, domain_size)
+        result = self.astargac.initialize()
+        if result == "SOL":
             self.draw()
-            return
-        else:
-            self.astar.n0.domains = copy.deepcopy(self.csp.domains)
-            self.redraw()
-        return
+        self.run_agac()
 
     # Draws the gui with nodes from the open, closed, and complete path list
-    def redraw(self):
-        #print self.csp.domains
-
-        # run astar
-        #astar_result = self.astar.incremental_solver()
-        #self.csp.domains = astar_result[1]
-        if self.csp.is_solved():
-            print "Solution found!"
+    def run_agac(self):
+        result = self.astargac.increment()
+        if result == "SOL":
             self.draw()
             self.print_solution()
             return
-        astar_result = self.astar.incremental_solver()
-        #print astar_result
-        if astar_result[0].startswith("SUCCESS"):
-            current_csp_domains = deepcopy(self.csp.domains)
-            found_better = False
+        elif result == "MOD":
+            self.draw()
+        elif result == "NON":
+            pass
 
-            for node in self.astar.open_heap:
-                if node not in self.nodes_checked: self.nodes_checked.add(node)
-                else: continue
-                self.csp.contradictory = False
-                # rerun csp and find best guess
-                self.csp.domains = node.domains
-                csp_rerun_result = self.csp.rerun()
-                #print csp_rerun_result[0]
-                if self.csp.is_solved():
-                    print "Solution found"
-                    self.draw()
-                    self.print_solution()
-                    self.goal_node = node
-                    return
-                if self.csp.contradictory:
-                    #print "CONTRADICTORY"
-                    self.astar.open_heap.remove(node)
-                    self.astar.open_set.remove(node)
-                    continue
-                else:
-                    # fix node
-                    node.domains = deepcopy(self.csp.domains)
-                    found_better = True
-                    node.set_f(g=0, h=self.astar.calculate_h(node))
-                    self.draw()
-            if found_better:
-                node = min(self.astar.open_heap, key=attrgetter('f'))
-                #node = self.astar.open_heap[0]
-                #print "best f", node.f
-                self.csp.domains = node.domains
-                self.goal_node = node
+        # Delay before next drawing phase
+        self.after(self.update_speed, lambda: self.run_agac())
 
-            else:
-                self.csp.domains = current_csp_domains
-
-            heapq.heapify(self.astar.open_heap)
-
-            # Delay before next drawing phase
-
-            self.after(self.update_speed, lambda: self.redraw())
-        else: return
 
     def draw(self):
-        for domain in self.csp.domains:
-                if len(self.csp.domains[domain]) == 1:
-                #if True:
+        for domain in self.astargac.csp.domains:
+                if len(self.astargac.csp.domains[domain]) == 1:
                     vertex = self.graph.vertices[domain]
                     item_id = self.oval[vertex[1], vertex[2]]
-                    #print self.csp.domains[domain][0]]
-                    self.canvas.itemconfig(item_id, fill=self.color_list[self.csp.domains[domain][0]])
+                    self.canvas.itemconfig(item_id, fill=self.color_list[self.astargac.csp.domains[domain][0]])
 
     def print_solution(self):
         null_vertices = 0
         unsatisfied_vertices = 0
-        for i, domain in self.csp.domains.iteritems():
+        for i, domain in self.astargac.csp.domains.iteritems():
             if len(domain) == 0: null_vertices += 1
             if len(domain) > 1: unsatisfied_vertices += 1
-        parent = self.goal_node.parent
+        parent = self.astargac.goal_node.parent
         parent_nodes = 0
         while parent:
             parent = parent.parent
@@ -189,8 +98,8 @@ class GUI(tk.Tk):
 
         print "Unsatisfied constraints:", unsatisfied_vertices
         print "Vertices without color:", null_vertices
-        print "Nodes in the search tree:", len(self.astar.open_heap)
-        print "Nodes that were expanded:", len(self.astar.closed_set)
+        print "Nodes in the search tree:", len(self.astargac.astar.open_heap)
+        print "Nodes that were expanded:", len(self.astargac.astar.closed_set)
         print "Length of path from start to goal:", parent_nodes
 
 
