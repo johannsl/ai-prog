237f48d06bd38d055d85313c2b0e62936029734e
Iver Egge <iverasp@gmail.com>
2015-10-14 15:23:03 +0200

    setup 2048 gui

---
+++ module_4-java/src/Main.java
@@ -0,0 +1,11 @@
+import game2048.Game2048;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Game2048 game = new Game2048();
+        game.main(null);
+
+    }
+}
