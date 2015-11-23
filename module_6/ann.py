c8c7a5ff471c332da8c9e6f4e6e2d1a8b5868bbe
Iver Egge <iverasp@gmail.com>
2015-11-23 16:02:10 +0100

    annie ann

--- module_6/ann.py
+++ module_6/ann.py
@@ -1,3 +1,6 @@
+# Written based on Newmu's Theano-Tutorial 
+# (https://github.com/Newmu/Theano-Tutorials)
+ 
 import load
 import numpy
 import theano
@@ -5,96 +8,162 @@ from theano import tensor
 from theano.sandbox.rng_mrg import MRG_RandomStreams
 
 # Constants
-NUMBER_OF_RUNS = 100
+NUMBER_OF_RUNS = 1
+INPUT_SIZE = 16
+OUTPUT_SIZE = 4
 LEARNING_RATE = 0.001
 RHO = 0.9
 EPSILON = 1e-6
+BATCH_SIZE = 100
 
 # Stream random number generator
 srng = MRG_RandomStreams()
 
-def float_x(x):
-    return numpy.asarray(x, dtype=theano.config.floatX)
+class ann():
+    def __init__(self, layer_sizes):
+        self.layer_sizes = layer_sizes
 
-def init_weights(shape):
-    return theano.shared(float_x(numpy.random.randn(*shape) * 0.01))
+    # Activation functions
+    def rectify(self, x):
+        return tensor.maximum(x, 0.)
+    
+    def sigmoid(self, x):
+        return tensor.nnet.sigmoid(x)
+    
+    def softmax(self, x):
+        e_x = tensor.exp(x - x.max(axis=1).dimshuffle(0, 'x'))
+        return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+    
+    # Help functions
+    def float_x(self, x):
+        return numpy.asarray(x, dtype=theano.config.floatX)
+    
+    def init_weights(self, shape):
+        return theano.shared(self.float_x(numpy.random.randn(*shape) * 0.01))
+    
+    # Backpropagation functions
+    def RMSprop(self, cost, params):
+        grads = tensor.grad(cost=cost, wrt=params)
+        updates = []
+        for param, grad in zip(params, grads):
+            accelerate = theano.shared(param.get_value() * 0.)
+            accelerate_new = RHO * accelerate + (1 - RHO) * grad ** 2
+            gradient_scaling = tensor.sqrt(accelerate_new + EPSILON)
+            grad = grad / gradient_scaling
+            updates.append((accelerate, accelerate_new))
+            updates.append((param, param - LEARNING_RATE * grad))
+        return updates
+    
+    def SGD(self, cost, params):
+        grads = tensor.grad(cost=cost, wrt=params)
+        updates = []
+        for param, grad in zip(params, grads):
+            updates.append([param, param - grad * LEARNING_RATE])
+        return updates
+    
+    # Noise function
+    def dropout(self, x, percent=0.):
+        if percent > 0:
+            retain_prob = 1 - percent
+            x *= srng.binomial(
+                            x.shape, 
+                            p=retain_prob, 
+                            dtype=theano.config.floatX)
+            x /= retain_prob
+        return x
+    
+    # The network models:
+    # simple network with sigmoid activation;
+    def model(self, x, weights, p_drop_input, p_drop_hidden):
+        model_layer_values = []
+        temp_layer_value = x
+        for weight in weights[:-1]:
+            layer_value = self.sigmoid(tensor.dot(temp_layer_value, weight))
+            model_layer_values.append(layer_value)
+            temp_layer_value = layer_value
+        layer_value = self.softmax(tensor.dot(temp_layer_value, weights[-1]))
+        model_layer_values.append(layer_value)
+        return model_layer_values
+    
+    # advanced network with rec activation and noise;
+    def model2(self, x, weights, p_drop_input, p_drop_hidden):
+        model_layer_values = []
+        x = self.dropout(x, p_drop_input)
+        temp_layer_value = x
+        for weight in weights[:-1]:
+            layer_value = self.rectify(tensor.dot(temp_layer_value, weight))
+            layer_value = self.dropout(layer_value, p_drop_hidden)
+            model_layer_values.append(layer_value)
+            temp_layer_value = layer_value
+        layer_value = self.softmax(tensor.dot(temp_layer_value, weights[-1]))
+        model_layer_values.append(layer_value)
+        return model_layer_values
+    
+    # Load, init, and run function
+    def main(self, silent=False):
+        
+        # Load training and test data
+        training_x, test_x, training_y, test_y = load.game2048()
+        
+        # Symbolic variables
+        x = tensor.fmatrix()
+        y = tensor.fmatrix()
+        
+        # Initialize weights
+        temp_weight = INPUT_SIZE
+        weights = []
+        for layer in self.layer_sizes:
+            weight = self.init_weights((temp_weight, layer))
+            weights.append(weight)
+            temp_weight = layer
+        weight = self.init_weights((temp_weight, OUTPUT_SIZE))
+        weights.append(weight) 
+        
+        # Initialize model
+        model_layer_noise = self.model2(x, weights, 0.2, 0.5)
+        model_layer_values = self.model2(x, weights, 0., 0.)
+        #y_x = tensor.argmax(model_layer_values[-1], axis=1)
+        y_x = model_layer_values[-1]
+        
+        # Initialize the update function
+        cost = tensor.mean(tensor.nnet.categorical_crossentropy(
+                                                model_layer_noise[-1], y))
+        params = weights
+        updates = self.RMSprop(cost, params) # SGD / RMSprop
 
-def rectify(x):
-    return tensor.maximum(x, 0.)
+        # Initialize core functionality
+        train = theano.function(
+                        inputs=[x, y],
+                        outputs=cost, 
+                        updates=updates,
+                        allow_input_downcast=True)
+        self.predict = theano.function(
+                        inputs=[x], 
+                        outputs=y_x, 
+                        allow_input_downcast=True)
+        
+        # Training on mnist
+        if not silent: print("\nTRAINING...")
+        for i in range(NUMBER_OF_RUNS):
+            for start, end in zip(range(0, len(training_x), BATCH_SIZE),
+                            range(BATCH_SIZE, len(training_x), BATCH_SIZE)):
+                cost = train(training_x[start:end], training_y[start:end])
+            if not silent: print("Iteration ", i+1, "/", NUMBER_OF_RUNS, "(", 
+                        numpy.mean(numpy.argmax(test_y, axis=1) 
+                        == self.predict(test_x))*100, ")")
+        
 
-def softmax(x):
-    e_x = tensor.exp(x - x.max(axis=1).dimshuffle(0, 'x'))
-    return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+    # Run a blind test
+    def blind_test(self, feature_sets):
+        print("\nBLIND TESTING...")
+        feature_sets = numpy.asarray(feature_sets)
+        feature_sets = feature_sets/255.
+        result = self.predict(feature_sets[0])
+        result = result.tolist()
+        print("Result: ", result)
+        return result
 
-def RMSprop(cost, params):
-    grads = tensor.grad(cost=cost, wrt=params)
-    updates = []
-    for p, g in zip(params, grads):
-        acc = theano.shared(p.get_value() * 0.)
-        acc_new = RHO * acc + (1 - RHO) * g ** 2
-        gradient_scaling = tensor.sqrt(acc_new + EPSILON)
-        g = g / gradient_scaling
-        updates.append((acc, acc_new))
-        updates.append((p, p - LEARNING_RATE * g))
-    return updates
+    def predict_move(self, board):
+        result = self.predict(board)
+        return result[0] 
 
-def dropout(x, p=0.):
-    if p > 0:
-        retain_prob = 1 - p
-        x *= srng.binomial(x.shape, p=retain_prob, dtype=theano.config.floatX)
-        x /= retain_prob
-    return x
-
-def model(x, weight_hidden, weight_hidden2, weight_out, p_drop_inumpyut, p_drop_hidden):
-    x = dropout(x, p_drop_inumpyut)
-    h = rectify(tensor.dot(x, weight_hidden))
-
-    h = dropout(h, p_drop_hidden)
-    h2 = rectify(tensor.dot(h, weight_hidden2))
-
-    h2 = dropout(h2, p_drop_hidden)
-    py_x = softmax(tensor.dot(h2, weight_out))
-    return h, h2, py_x
-
-# Training and test data; 
-# x represents the data and y represents the correct response
-training_x, test_x, training_y, test_y = load.game2048()
-
-# Symbolic variables
-x = tensor.fmatrix()
-y = tensor.fmatrix()
-
-weight_hidden = init_weights((16, 1200))
-weight_hidden2 = init_weights((1200, 1200))
-weight_out = init_weights((1200, 4))
-
-noise_h, noise_h2, noise_py_x = model(x, weight_hidden, weight_hidden2, weight_out, 0.2, 0.5)
-h, h2, py_x = model(x, weight_hidden, weight_hidden2, weight_out, 0., 0.)
-
-cost = tensor.mean(tensor.nnet.categorical_crossentropy(noise_py_x, y))
-params = [weight_hidden, weight_hidden2, weight_out]
-updates = RMSprop(cost, params)
-
-# This is the core of the ann functionality
-train = theano.function(inputs=[x, y], outputs=cost, updates=updates, allow_input_downcast=True)
-predict = theano.function(inputs=[x], outputs=py_x, allow_input_downcast=True)
-
-# Run training and testing
-def run(silent=False):
-    numpy.set_printoptions(threshold=numpy.nan)
-    #for i in range(NUMBER_OF_RUNS):
-    n = 10
-    for i in range(n):
-        if not silent: print("Training iteration", i+1, "of", n)
-        for start, end in zip(range(0, len(training_x), 128),
-                             range(128, len(training_y), 128)):
-            cost = train(training_x[start:end], training_y[start:end])
-        #print(numpy.mean(numpy.argmax(test_y, axis=1) == predict(test_x)))
-        #print(test_y[0], predict(test_x)[0])
-        #print(len(predict(test_x)))
-
-def predict_move(board):
-    #print("board", board)
-    result = predict(board)
-    #print("result", result)
-    return result[0]
